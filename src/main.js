364a1380fa483f239e3e43103c35c819e2cb533f
Daniel Alas <alasalexander76@gmail.com>
2025-05-20 17:01:54 -0600

    Cambios de diseño, pantalla principal agregada y funcion para iniciar quizz

--- src/main.js
+++ src/main.js
@@ -1,24 +1,13 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+function startQuiz(tema) {
+  document.getElementById('pantallaPrincipal').style.display = 'none';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  const quizSection = document.getElementById('contenedorPreguntas');
+  quizSection.style.display = 'grid';
 
-setupCounter(document.querySelector('#counter'))
+  const textoTema = document.getElementById('textoTema');
+  textoTema.textContent = tema.toUpperCase(); 
+
+  console.log("Tema seleccionado:", tema);
+}
+
+window.startQuiz = startQuiz;
