021559c86add46a27fd76b61040540de5fd901e8
Daniel Alas <alasalexander76@gmail.com>
2025-05-21 18:05:56 -0600

    Funcion de interruptor añadida, organizacion de documento y aplicación de BEM agregadas

--- src/main.js
+++ src/main.js
@@ -11,3 +11,20 @@ function empezarQuiz(tema) {
 }
 
 window.empezarQuiz = empezarQuiz;
+
+const toggle = document.getElementById('checkbox');
+const root = document.documentElement;
+const iconoSol = document.querySelector('.header__icono-sol img');
+const iconoLuna = document.querySelector('.header__icono-luna img');
+
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    root.classList.add('modo-claro');
+    iconoSol.src = '/src/images/icon-sun-dark.svg';
+    iconoLuna.src = '/src/images/icon-moon-dark.svg';
+  } else {
+    root.classList.remove('modo-claro');
+    iconoSol.src = '/src/images/icon-sun-light.svg';
+    iconoLuna.src = '/src/images/icon-moon-light.svg';
+  }
+});
